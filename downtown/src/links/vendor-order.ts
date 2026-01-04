41163d1b753e0ea286856aa300556b4dbe6a8b25
able <abenable@outlook.com>
2026-01-04 12:38:51 +0300

    fix: update linkable references in vendor-order and vendor-product to use IDs

--- downtown/src/links/vendor-order.ts
+++ downtown/src/links/vendor-order.ts
@@ -3,6 +3,6 @@ import MarketplaceModule from "../modules/marketplace";
 import OrderModule from "@medusajs/medusa/order";
 
 export default defineLink(MarketplaceModule.linkable.vendor, {
-  linkable: OrderModule.linkable.order,
+  linkable: OrderModule.linkable.order.id,
   isList: true,
 });
--- downtown/src/links/vendor-product.ts
+++ downtown/src/links/vendor-product.ts
@@ -3,6 +3,6 @@ import MarketplaceModule from "../modules/marketplace";
 import ProductModule from "@medusajs/medusa/product";
 
 export default defineLink(MarketplaceModule.linkable.vendor, {
-  linkable: ProductModule.linkable.product,
+  linkable: ProductModule.linkable.product.id,
   isList: true,
 });
